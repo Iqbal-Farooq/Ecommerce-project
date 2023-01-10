6ec7f742c9a35e5c80950b2d6e3c8a3cb5f01076
iqbal-Farooq <akashfarooq222@gmail.com>
2023-01-10 10:32:25 +0530

    Fetch data when component loads using useEffect Hook

--- src/DataFetch/Appp.js
+++ src/DataFetch/Appp.js
@@ -1,15 +1,31 @@
 
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import {Button,Col} from "react-bootstrap"
 
 const Appp=()=>{
      const [state,SetState]=useState([])
      const[loader,SetLoader]=useState(false);
+     const[error,SetError]=useState(false);
+    
+   
 
-   async function FetchMovieHandler(){
+     
+
+   const  FetchMovieHandler=useCallback(async()=>{
     SetLoader(true);
-       
-        const response=await fetch('https://swapi.dev/api/films/');
-        const data=await response.json();
+    SetError(null);
+    
+   
+    try{
+      
+         let response=await fetch('https://swapi.dev/api/films/');
+
+             if(!response.ok){
+                 
+            throw new Error("SOMETHING WENT WRONG Retrying...");
+        }
+
+  const data=await response.json();
      
             const movies=data.results.map((movieData)=>{return {
                 id:movieData.episode_id,
@@ -21,21 +37,35 @@ const Appp=()=>{
            
         });
          SetState(movies);
-         SetLoader(false)
+      
+    }catch(error){
+        SetError(error.message);
+
     }
+       SetLoader(false)
+  },[])
+
+    useEffect(() => {
+        FetchMovieHandler();
+	
+	},[FetchMovieHandler]);
+   
+ 
 
     return(<>
 
-    <header><button onClick={FetchMovieHandler}>fetch movies</button></header>
+    <header><Button variant="success" onClick={FetchMovieHandler}>fetch movies</Button></header>
 
      <main>  <li><span>movieId </span> <span> MovieTitle </span> <span> MovieReleaseDate </span></li>
 
-     {loader && <p style={{color:"Green"}}>Loadingn ⏳⏳⏳⏳</p>}
-     {!loader && state.map((movie)=>{return <ul> <li>{movie.id} {movie.title} {movie.releaseDate}</li> </ul>})}</main>
-
+     {loader && <> <p style={{color:"Green"}}>Please Wait....⏳⏳⏳⏳</p>,<button onClick={()=>console.log("clicked")}>cancel</button></> }
+     
+     {!loader && state.map((movie)=>{return <ul> <li key={movie.id}>{movie.id} {movie.title} {movie.releaseDate}</li> </ul>})}</main>
+            {!loader && error &&<>  <p>{error}</p> </>}
 
 
     </>)
 
 }
 export default Appp;
+
